fae54f74de21621e56a64c5f15743f9df39ee137
PineappleGinge <daraorourke209@gmail.com>
2025-01-08 13:35:54 +0000

    Added IComparable and attempted the second part of Q1g

--- JanExam/Event.cs
+++ JanExam/Event.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JanExam
 {
-    public class Event
+    public enum EventType { Music, Comedy, Theatre }
+    internal class Event : IComparable
     {
         //Properties
         public string Name { get; set; }
         public DateTime EventDate { get; set; }
         public List<Ticket> Tickets { get; set; }
 
-        public enum EventType { Music, Comedy, Theatre }
-
+        
         // constructors
 
 
         //methods
+        public int CompareTo(object obj)
+        {
+            Event @event;
+            return @event.CompareTo(EventDate);
+        }
+
 
     }
 }
