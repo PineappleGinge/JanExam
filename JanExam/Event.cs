734de5bd9705b65ac09f79b8cc3793ed1c28fe9c
PineappleGinge <daraorourke209@gmail.com>
2025-01-08 13:27:36 +0000

    added properites to the event class

---
+++ JanExam/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanExam
+{
+    public class Event
+    {
+        //Properties
+        public string Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public List<Ticket> Tickets { get; set; }
+
+        public enum EventType { Music, Comedy, Theatre }
+
+        // constructors
+
+
+        //methods
+
+    }
+}
